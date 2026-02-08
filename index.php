f8da8f077ccdefb241cbea440740e88958d13103
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2026-02-08 14:35:04 +0100

    Language Option Prep
    Small changes to css for design dining/language options, fixed issue with body margin & prep for the language option saving. will probably do so aswell for other things or might rewrite later since that bit will probably become redundant maybe?

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 session_start();
 
 include_once 'connection.php';
+
+$languageChoice = 'Dutch';
+
+//prep for language switching. default is dutch, will rewrite later
+if (isset($_SESSION['languageOption'])){
+    if ($_SESSION['languageOption'] == 'English') {
+        $languageChoice = 'English';
+    } elseif ($_SESSION['languageOption'] == 'Dutch') {
+        $languageChoice = 'Dutch';    
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,44 +31,28 @@ include_once 'connection.php';
     <main class="start-page">
         <img class="bigLogo" src="assets/img/logo_big_complete_transparent.webp" alt="big logo">
 
-        <div class="diningOptions">
+        <!-- <div class="diningOptions">
             <div id="DiningIn" class="option">
-                <img src="assets/img/eatInIcon.png" alt="eat in icon"> <br>
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/eatInIcon.png" alt="eat in icon"> <br>
                 <p>Dine In</p>
             </div>
             <div id="DiningOut" class="option">
-                <img src="assets/img/takeOutIcon.png" alt="take out icon"><br>
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/takeOutIcon.png" alt="take out icon"><br>
                 <p>Take Out</p>
+            </div>
+        </div> -->
+
+        <div class="languageOptions">
+            <div id="EnglishOption" class="option">
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/ENicon.png" alt="eat in icon"> <br>
+                <p>English</p>
+            </div>
+            <div id="DutchOption" class="option">
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/NLIcon.png" alt="take out icon"><br>
+                <p>Dutch</p>
             </div>
         </div>
 
-        <!-- <div class="options">
-            <h1>Where will you be eating today?</h1>
-
-            <div class="dining-options">
-                <form action="menu.php" method="post">
-                    <input type="hidden" name="dineIn" value="1">
-                    <button class="button" type="submit" name="dineChoiceButton">
-                        <img class="option-img" src="assets/img/tray.png" alt="tray">
-                        <div class="text-and-arrow">
-                            <p>Dine in</p>
-                            <img class="arrow" src="assets/img/arrow-right.svg" alt="arrow-right">
-                        </div>
-                    </button>
-                </form>
-
-                <form action="menu.php" method="post">
-                    <input type="hidden" name="dineOut" value="2">
-                    <button class="button" type="submit" name="dineChoiceButton">
-                        <img class="option-img" src="assets/img/paper-bag.png" alt="paper-bag">
-                        <div class="text-and-arrow">
-                            <p>Take out</p>
-                            <img class="arrow" src="assets/img/arrow-right.svg" alt="arrow-right">
-                        </div>
-                    </button>
-                </form>
-            </div>
-        </div> -->
     </main>
 
 </body>
