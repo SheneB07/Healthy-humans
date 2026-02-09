a97f503074452ecb2cba86d2b92740fc89cd1c9e
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2026-02-09 16:07:32 +0100

    Working starting screen
    Fully working starting screen using AJAX and session to save the language and dining options inside session.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_destroy();
 
 include_once 'connection.php';
 
@@ -23,7 +24,7 @@ if (isset($_SESSION['languageOption'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="assets/css/style.css">
-            <link rel="stylesheet" href="assets/css/startscherm.css">
+    <link rel="stylesheet" href="assets/css/startscherm.css">
     <title>Start</title>
 </head>
 
@@ -32,33 +33,32 @@ if (isset($_SESSION['languageOption'])) {
     <main class="start-page">
         <img class="bigLogo" src="assets/img/logo_big_complete_transparent.webp" alt="big logo">
 
-        <!-- prep for full start screen workings. Will need to work with covers to first cover all this, then reveal the language options and
- let user make the choice, and then cover language options again and reveal dining options, which then saves the choices and sends
- the user to the menu screen. save via session.-->
-
-        <!-- <div class="diningOptions">
-            <div id="DiningIn" class="option">
-                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/eatInIcon.png" alt="eat in icon"> <br>
-                <p>Dine In</p>
-            </div>
-            <div id="DiningOut" class="option">
-                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/takeOutIcon.png" alt="take out icon"><br>
-                <p>Take Out</p>
-            </div>
-        </div> -->
-
-        <div class="languageOptions">
-            <div id="EnglishOption" class="option">
+         <div id="languageOptions">
+            <div id="EnglishOption" data-language="English" class="languageOption">
                 <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/ENicon.png" alt="eat in icon"> <br>
                 <p>English</p>
             </div>
-            <div id="DutchOption" class="option">
+
+            <div id="DutchOption" data-language="Dutch" class="languageOption">
                 <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/NLIcon.png" alt="take out icon"><br>
                 <p>Dutch</p>
             </div>
         </div>
 
+        <div id="diningOptions">
+            <div id="DiningIn" data-dining="DineIn" class="diningOption">
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/eatInIcon.png" alt="eat in icon"> <br>
+                <p>Dine In</p>
+            </div>
+
+            <div id="DiningOut" data-dining="TakeOut" class="diningOption">
+                <img style="width: 90%; height: auto; margin-top: 15%;" src="assets/img/takeOutIcon.png" alt="take out icon"><br>
+                <p>Take Out</p>
+            </div>
+        </div>
+
     </main>
+    <script src="assets/js/startscherm.js"></script>
 
 </body>
 
