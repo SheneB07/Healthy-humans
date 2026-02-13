7ffe7a8efeab442aafa59ee92a4729e8cfd85582
SheneB07 <shenebanga@gmail.com>
2026-02-13 10:39:15 +0100

    basis van menu scherm

---
+++ menu.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="topbar-container">
+        <div class="top-left">
+            <img src="assets/img/logo_big_happy_herbivore_transparent.png" id="menu-logo">
+        </div>
+        <div class="top-right">
+            <div id="menu-topbox">
+                <h2>Breakfast</h2>
+            </div>
+        </div>
+    </div>
+    <div class="content-container">
+        <div class="categories-box">
+            <div id="option-button">
+                <img src="assets/img/breakfast-icon.png" id="category-icon">
+                <h1>Highlights</h1>
+            </div>
+            <div id="option-button">
+                <img src="assets/img/salad-icon.png" id="category-icon">
+                <h1>Lunch & Diner</h1>
+            </div>
+            <div id="option-button">
+                <img src="assets/img/sandwich-icon.png" id="category-icon">
+                <h1>Handhelds</h1>
+            </div>
+            <div id="option-button">
+                <img src="assets/img/sides-icon.png" id="category-icon">
+                <h1>Sides</h1>
+            </div>
+            <div id="option-button">
+                <img src="assets/img/dips-icon.png" id="category-icon">
+                <h1>Dips</h1>
+            </div>
+            <div id="option-button"></div>
+            <div id="option-button"></div>
+        </div>
+        <div class="menu-items"></div>
+    </div>
+    <div class="bottombar-container">
+        <div id="pink-bar"></div>
+    </div>
+
+</body>
+</html>
