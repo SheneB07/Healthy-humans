439a68c9b8aefd7af615614537906b98696ba44f
SheneB07 <shenebanga@gmail.com>
2026-02-13 13:26:08 +0100

    edits + menu foto's

--- menu.php
+++ menu.php
@@ -1,3 +1,22 @@
+<?php
+include("connection.php");
+
+try {
+    // Prepare the query to select the name and description
+    $stmt = $pdo->prepare("SELECT * FROM products");
+    $stmt->execute();
+
+    // Set the fetch mode to associative array
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $products = $stmt->fetchAll();  // This stores the result in the variable you use below
+
+    
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$pdo = null;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
     <div class="topbar-container">
         <div class="top-left">
@@ -19,8 +39,12 @@
     <div class="content-container">
         <div class="categories-box">
             <div id="option-button">
+                <img src="assets/img/highlights.png" id="category-icon">
+                <h1>Full menu</h1>
+            </div>
+            <div id="option-button">
                 <img src="assets/img/breakfast-icon.png" id="category-icon">
-                <h1>Highlights</h1>
+                <h1>Breakfast</h1>
             </div>
             <div id="option-button">
                 <img src="assets/img/salad-icon.png" id="category-icon">
@@ -38,10 +62,28 @@
                 <img src="assets/img/dips-icon.png" id="category-icon">
                 <h1>Dips</h1>
             </div>
-            <div id="option-button"></div>
-            <div id="option-button"></div>
+            <div id="option-button">
+                <img src="assets/img/drink-icon.png" id="category-icon">
+                <h1>Drinks</h1>
+            </div>
         </div>
-        <div class="menu-items"></div>
+
+        
+
+<?php
+    foreach($products as $product):
+?>
+        <div class="menu-items">
+            <div class="menu-item">
+                <h3><?= $product['name']; ?></h3>
+            </div>
+<?php 
+    endforeach;
+?>
+            <div class="menu-item">
+                
+            </div>
+        </div>
     </div>
     <div class="bottombar-container">
         <div id="pink-bar"></div>
